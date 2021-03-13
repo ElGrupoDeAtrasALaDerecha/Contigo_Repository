1adccb6dde551c5b6d1a94e839bec66ab89cece2
Santiago43 <santipego0001@gmail.com>
2021-03-12 20:02:26 -0500

    Se agregan funciones web, y mensajes de bot

--- Contigo/web/js/bot.js
+++ Contigo/web/js/bot.js
@@ -1,3 +1,5 @@
+var numeroSala;
+
 /**
 * Promesa
 * @param {*} ms 
@@ -13,13 +15,21 @@ var wsUri = "ws://localhost:30001";
 var websocket = new WebSocket(wsUri);
 websocket.onopen = function(event){
 	console.log("Conectado..."); //... y aparecerá en la pantalla
-	var datos ={
-        tipo:"datos",
-        nombre:"pablito",
-        segundoNombre:"Emilio"
-    }
-    enviarMensaje(datos);
+	enviarCredenciales();
     ping();
+}
+
+websocket.onmessage=function(event){
+	if (event.data==="pong"){
+		ping();
+	}else{
+		let obj=JSON.parse(event.data);
+		if(obj.tipo==="numero sala"){
+			numeroSala=obj.numero;
+		}else if(obj.tipo==="respuesta"){
+			pintarRespuesta(obj.mensaje);
+		}
+	}
 }
 
 
@@ -34,4 +44,39 @@ function ping(){
 	var prom = wait(28000)  // prom, is a promise
 	var showdone = () => enviarMensaje(myPing);
 	prom.then(showdone)
+}
+
+
+/**
+ * Se envían credenciales al bot para la identificación
+ */
+function enviarCredenciales(){
+	let datos={
+		tipo:"primer ingreso",
+		token: getCookie("token")
+	}
+	enviarMensaje(datos);
+}
+
+/**
+ * Función que muestra en la pantalla la respuesta del servidor
+ * @param {string} respuesta 
+ */
+
+function pintarRespuesta(respuesta){
+	console.log(respuesta);
+}
+
+
+
+/**
+ * Información a conti
+ */
+ function decirleAConti(mensaje){
+	let datos={
+		tipo:"mensaje",
+		"numero sala": "1",
+		mensaje: mensaje
+	}
+	enviarMensaje(datos);
 }
