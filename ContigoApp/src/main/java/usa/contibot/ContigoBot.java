a9b1e81a03fbdc73076023c85580bb0c68e50942
Santiago43 <santipego0001@gmail.com>
2021-03-13 22:24:38 -0500

    Se agrega bot contigo

--- ContigoApp/src/main/java/usa/contibot/ContigoBot.java
+++ ContigoApp/src/main/java/usa/contibot/ContigoBot.java
@@ -1,15 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package usa.contibot;
 
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.websocket.OnClose;
-import javax.websocket.OnError;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
