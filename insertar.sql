ea30f67830be30222acdba2e6ae3b7805f8e5e0c
Santiago43 <santipego0001@gmail.com>
2021-03-16 18:51:52 -0500

    Se agregan scripts sql importantes

--- insertar.sql
+++ insertar.sql
@@ -1,3 +1,9 @@
 insert into TIPO_DOCUMENTO (tipo) values ("Cédula de ciudadanía");
 
-call insertarPersonalCalificado(  "1000853623",1,"Santiago","","Pérez" ,"González","1234","2000-07-26",sha("1234"),"masculino","santipego0001@gmail.com");
+call insertarPersonalCalificado(  "1000853623",1,"Santiago","","Pérez" ,"González","1234","2000-07-26","1234","masculino","santipego0001@gmail.com");
+
+
+call insertarPersonalCalificado("1000853622",1,"Valeria","","Bermúdez" ,"González","1234","2000-07-26","1234","femenino","aja@gmail.com");
+call insertarEstudiante("1000853621",1,"Valeria","","Bermúdez" ,"González","1234","2000-07-26","1234","femenino",1);
+
+
