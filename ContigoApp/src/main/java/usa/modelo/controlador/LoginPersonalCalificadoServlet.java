2adef1372009ebd05edec5c6c5d3d79561997f22
Santiago43 <santipego0001@gmail.com>
2021-03-16 21:50:44 -0500

    Se agregan servlets corregidos de personal calificado

--- ContigoApp/src/main/java/usa/modelo/controlador/LoginPersonalCalificadoServlet.java
+++ ContigoApp/src/main/java/usa/modelo/controlador/LoginPersonalCalificadoServlet.java
@@ -28,32 +28,6 @@ import usa.utils.Utils;
 public class LoginPersonalCalificadoServlet extends HttpServlet {
 
     /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet LoginPersonalCalificadoServlet</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet LoginPersonalCalificadoServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    /**
      * Handles the HTTP <code>POST</code> method.
      *
      * @param request servlet request
@@ -64,7 +38,6 @@ public class LoginPersonalCalificadoServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
         PrintWriter out = response.getWriter();
         response.setContentType("application/json;charset=UTF-8");
         String parametros = Utils.readParams(request);
