d12f59401e6442e062f3ae20fa698afbf1307267
Santiago43 <santipego0001@gmail.com>
2021-03-21 13:29:55 -0500

    Se agrega botón de pánico-ayuda

--- ContigoApp/src/main/webapp/js/ayuda.js
+++ ContigoApp/src/main/webapp/js/ayuda.js
@@ -1,7 +1,17 @@
-$('#btnAyuda')
-    .popup()
-  ;
 
-$("#btnAyuda").click(function(){
-    window.location.assign("chat.html");
-})
+$(document).ready(function(){
+    let txt=`<button id="btnAyuda" class="red inverted circular massive ui icon button"  data-content="¿Necesitas ayuda?">
+                <i class="icon heartbeat"></i>
+            </button>`
+    $("body").append(txt);
+    $('#btnAyuda').popup();
+    $("#btnAyuda").css({
+        "position": "fixed",
+        "z-index": "10000",
+        "top": "90%",
+        "left": "95%"
+      });
+    $("#btnAyuda").click(function(){
+        window.location.assign("chat.html");
+    })
+});
