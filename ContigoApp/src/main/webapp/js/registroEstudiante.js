acc0775d6d1adc86d126e596cff35318fba010be
caceres908 <sacali99@gmail.com>
2021-03-22 09:44:53 -0500

    Se arregla problemas en registro estudiantes

--- ContigoApp/src/main/webapp/js/registroEstudiante.js
+++ ContigoApp/src/main/webapp/js/registroEstudiante.js
@@ -1,3 +1,35 @@
+var identificaciones ;
+
+window.onload = function tipos_id(){
+    $.ajax({
+        url: "TipoDocumento",
+        type: "GET",
+        dataType: "json",
+        success: function (result, textStatus, request) {
+            if (result != "error") {
+                console.log(result);
+                identificaciones = result.Identificaciones;
+                for (var i = 0; i < identificaciones.length; i++) {
+                    let departa = '<option value ="' + identificaciones[i].id + '">' + identificaciones[i].tipo +
+                        '</option>';
+                    $("#TipoDocumento").append(departa);
+                }
+            } else {
+                console.log("error");
+            }
+
+        },
+        complete: function (result) {
+
+
+        },
+        error: function (result) {
+
+        }
+
+    });
+} 
+
 function registrar_estudiante() {
     tidocu = $("#TipoDocumento").val();
     numdocu = $("#TIoCC").val();
@@ -52,4 +84,32 @@ function registrar_estudiante() {
     });
 
 
+}
+
+
+
+function Ingresar() {
+    TipoDocumento = document.getElementById("TipoDocumento").value;
+    TIoCC = document.getElementById("TIoCC").value;
+    PrimerNombre = document.getElementById("PrimerNombre").value;
+    PrimerApellido = document.getElementById("PrimerApellido").value;
+    Genero = document.getElementById("Genero").value;
+    CorreoElectronico = document.getElementById("CorreoElectronico").value;
+    CodigoInstitucional = document.getElementById("CodigoInstitucional").value;
+    SegundoApellido = document.getElementById("SegundoApellido").value;
+    FechaNacimiento = document.getElementById("FechaNacimiento").value;
+    contra = document.getElementById("contra").value;
+    conficontra = document.getElementById("conficontra").value;
+    var expReg= /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+    var esValido=expReg.test(CorreoElectronico);
+
+    if (TipoDocumento == "Tipo de documento" || TIoCC == "" || PrimerNombre == "" || PrimerApellido == "" || Genero == "Genero" || CorreoElectronico == "" || CodigoInstitucional == "" || SegundoApellido == "" || FechaNacimiento == "" || contra == "" || conficontra == "") {
+        alert("Todos los campos son obligatorios.");
+    } else if (contra != conficontra) {
+        alert("No coincide su contraseña con la confirmación");
+    } else if(esValido!=true){
+        alert("El correo es invalido")
+    }else{
+        registrar_estudiante();
+    }
 }
