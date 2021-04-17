362f7c2887284523f182a8bca56c7a093d1538a2
camilaFernandez <maria.fernandezo1@correo.usa.edu.co>
2021-04-17 18:42:42 -0500

    vista prueba, con js Vestadisticas y css prueba

--- ContigoApp/src/main/webapp/js/Vestadisticas.js
+++ ContigoApp/src/main/webapp/js/Vestadisticas.js
@@ -1,17 +1,72 @@
-const df = document.getElementById('opcbtn');
-df.style.display = "none"
+var opcGrado = document.getElementById('opcGrado');
+var divge = document.getElementById('ge');
+var btnDatos = document.getElementById('btn');
+var opcVisualizar;//Select de opcion a visualizar tipo de graficas a analizar 
+var grados;
+var listaEstudiantes= document.getElementById('ListaEstudiantes');;
+var arregloEstudiantes;
+opcGrado.style.display = "none"
+btnDatos.style.display = "none"
+listaEstudiantes.style.display = "none"
 function obtenerSelect() {
     /* Para obtener el valor */
-    var tex = document.getElementById("modulos").value;
-    if (tex === "1") {
-        df.style.display = "block"
-    } else if (tex !== "1") {
-        df.style.display = "none"
+     opcVisualiza = document.getElementById("consulta").value;
+    if (opcVisualiza === "1" || opcVisualiza ==="2") {
+        opcGrado.style.display = "block"
+        divge.style.display = "none"
+        
+        
+    } else if (opcVisualiza === "") {
+        opcGrado.style.display = "none"
     }
-    /* Para obtener el texto 
-    var combo = document.getElementById("modulos");
-    var selected = combo.options[combo.selectedIndex].text;
-    alert(selected);*/
+}
+
+function selects(){
+    grados = document.getElementById("grados").value;
+    var tiempo= document.getElementById("frecuenciaEstadisticas").value;
+    if (grados !== "" && tiempo !== "") {
+        btnDatos.style.display = "block"
+        listaEstudiantes.style.display = "block"
+        
+    } else {
+        btnDatos.style.display = "none"
+    }
+}
+
+function traerEstudiantes(){
+    if(opcVisualizar === "2"){
+        if(grados !==""){
+            $.ajax({
+                method: 'GET',
+                url: 'EstudiantePorGradoServlet?grado='+grados,
+                dataType: "json",
+                contentType: 'JSON application/json charset=utf-8',
+                headers:{
+                    token:getCookie("token")
+                }, 
+                success: function (response) {
+                    if (response.tipo === "ok") {
+                        arregloEstudiantes = response.estudiantes;
+                        llenarSelect();
+                        
+                    }else{
+                        console.log(response.mensaje);
+                    }
+                },
+                error: function (response) {
+                    console.log("Error: " + response.mensaje)
+                    
+                }
+            });
+        }
+    }
+}
+
+function llenarSelect(){
+    for(var i=0;i<arregloEstudiantes.length;i++){
+        let txt=`<option value="${arregloEstudiantes[i].documento}">${arregloEstudiantes[i].primerNombre} ${arregloEstudiantes[i].segundoNombre} ${arregloEstudiantes[i].primerApellido} ${arregloEstudiantes[i].segundoApellido}</option>`
+        $("#estudiantes").append(txt);
+    }
 }
 
 $("#btnGerar").on("click", function () {
