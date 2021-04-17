1e6045082482dfab3d684314607cbd5b7728c7e5
Santiago43 <santipego0001@gmail.com>
2021-04-17 18:47:39 -0500

    Se agrega método listar todas las historias

--- ContigoApp/src/main/java/usa/modelo/dao/HistoriaDao.java
+++ ContigoApp/src/main/java/usa/modelo/dao/HistoriaDao.java
@@ -31,7 +31,28 @@ public class HistoriaDao implements IHistoriasDao{
 
     @Override
     public LinkedList<Historia> listarTodos() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        LinkedList<Historia> historia= new LinkedList();
+        try {
+            String sql = "select * from HISTORIA;";
+            PreparedStatement pat = conn.prepareStatement(sql);
+            ResultSet rs = pat.executeQuery();
+            while (rs.next()) {
+                Estudiante estudiante = new Estudiante();
+                estudiante.setPrimerApellido("p.primerNombre");
+                estudiante.setDocumento(rs.getString("p.documento"));
+                estudiante.setPrimerNombre(rs.getString("p.primerNombre"));
+                estudiante.setSegundoNombre(rs.getString("p.segundoNombre"));
+                estudiante.setPrimerApellido(rs.getString("p.primerApellido"));
+                estudiante.setSegundoApellido(rs.getString("p.segundoApellido"));
+                estudiante.setFechaDeNacimiento(rs.getDate("p.fechaNacimiento").toString());
+                estudiante.setGenero(rs.getString("p.genero"));
+                estudiantes.add(estudiante);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(EstudianteDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return estudiantes;
     }
     
 }
