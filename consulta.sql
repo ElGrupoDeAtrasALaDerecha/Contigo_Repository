514bb21d7f53df0b2a2a9e6d2c723bc5c94cd6de
natis montenegro <natalia.montenegro01@correo.usa.edu.co>
2021-04-20 14:52:36 -0500

    Se crean las consultas para graficas estadisticas conversatorio

--- consulta.sql
+++ consulta.sql
@@ -22,3 +22,44 @@ select * from institucion where correo="contigoapp@gmail.com" and contraseña="c
 select * from grado;
 select * from conversatorio;
 select * from institucion;
+
+
+
+#Consultas Natalia Estadisticas conversatorios
+#Se consulta el id y el titulo de los conversatorios
+select C.id , C.titulo from  ESTUDIANTE_has_CONVERSATORIO as EC
+inner join conversatorio as C on EC.CONVERSATORIO_id = C.id;
+
+#Se consula cuantas personas se han registrado en los conversatorios, se agrupan los datos 
+#Inscritos por institución
+select C.titulo, count(EC.ESTUDIANTE_PERSONA_documento) as Inscritos from  ESTUDIANTE_has_CONVERSATORIO as EC
+right join conversatorio as C on EC.CONVERSATORIO_id = C.id 
+inner join estudiante as E on EC.ESTUDIANTE_PERSONA_documento = E.PERSONA_documento
+inner join grado as G on E.GRADO_codigo = G.codigo
+inner join institucion as I on G.INSTITUCION_id = I.id
+where I.id = 1
+group by C.id
+order by Inscritos desc
+limit 5
+;
+
+#Se consula cuantas personas se han registrado en los conversatorios, se agrupan los datos 
+#Inscritos por grado
+select C.titulo, count(EC.ESTUDIANTE_PERSONA_documento) as Inscritos from  ESTUDIANTE_has_CONVERSATORIO as EC
+inner join estudiante as E on EC.ESTUDIANTE_PERSONA_documento = E.PERSONA_documento
+inner join grado as G on E.GRADO_codigo = G.codigo
+right join conversatorio as C on EC.CONVERSATORIO_id = C.id 
+where G.codigo = "aaaaa" 
+group by C.id
+order by Inscritos desc
+limit 5
+; 
+
+
+
+
+
+
+
+
+
