4f2e8d0eae8b8f2f2f85b6498cb3342002bb413b
Santiago43 <santipego0001@gmail.com>
2021-04-21 10:22:13 -0500

    Se agrega archivo de consulta sql

---
+++ consulta.sql
@@ -0,0 +1,26 @@
+select * from Persona;
+
+
+select p.*,pc.* from Persona as p
+inner join Personal as pc on pc.PERSONA_documento=p.documento;
+
+select p.*,e.* from Persona as p
+inner join Estudiante as e on e.PERSONA_documento=p.documento;
+
+
+select * from Estudiante;
+
+
+select * from PERSONA;
+select * from CLASIFICACION_has_CONVERSATORIO;
+select * from PERSONAL;
+select * from clasificacion;
+select * FROM ESTUDIANTE;
+select * from institucion;
+
+select * from institucion where correo="contigoapp@gmail.com" and contraseña="contigo123";
+select * from grado;
+select * from conversatorio;
+select * from institucion;
+
+select * from historia;
