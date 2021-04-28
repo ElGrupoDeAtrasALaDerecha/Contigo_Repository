eb4d5bcf92de07b2e4bd0ea6898a2b93c7dffbb2
caceres908 <sacali99@gmail.com>
2021-04-27 22:07:42 -0500

    Se arregla problemas con el css de listado de historias (lau)

--- ContigoApp/src/main/java/usa/bd/Conexion.java
+++ ContigoApp/src/main/java/usa/bd/Conexion.java
@@ -13,7 +13,7 @@ import usa.bd.IConexionBD;
 public class Conexion extends IConexionBD{
     String bd = "contigoBD";
     String user= "root";
-    String password="1234";
+    String password="123456";
     String host="localhost";
     String port="3306";
     private final String url = "jdbc:mysql://"+host+":"+port+"/"+bd+"?user="+user+"&password="+password+"&serverTimezone=UTC&autoReconnect=true&useSSL=false";
