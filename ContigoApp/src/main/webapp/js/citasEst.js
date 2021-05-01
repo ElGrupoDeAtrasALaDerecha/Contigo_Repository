180ff8f5d7c4d673c777e20cf18f5ffedba80705
camilaFernandez <maria.fernandezo1@correo.usa.edu.co>
2021-05-01 00:42:28 -0500

    select de horas y color botones

--- ContigoApp/src/main/webapp/js/citasEst.js
+++ ContigoApp/src/main/webapp/js/citasEst.js
@@ -1,4 +1,4 @@
-let monthNames = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre','Octubre', 'Noviembre', 'Diciembre'];
+let monthNames = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
 
 let currentDate = new Date();//fecha del pc como ref
 let currentDay = currentDate.getDate();//dia de la semana
@@ -15,131 +15,160 @@ let nextMonthDOM = document.getElementById('next-month');
 month.textContent = monthNames[monthNumber];
 year.textContent = currentYear.toString();
 
-prevMonthDOM.addEventListener('click',mesAnterior);
-nextMonthDOM.addEventListener('click',mesSiguiente);
+prevMonthDOM.addEventListener('click', mesAnterior);
+nextMonthDOM.addEventListener('click', mesSiguiente);
 
 escribirMeses(monthNumber);
 var fecha;
-function escribirMeses(month){
-    var mesActual=3;
-    if(month < 3){
-        for(let i =inicioDia();i>0;i--){
+
+
+function escribirMeses(month, botonPresionado) {
+    if (month < 3) {
+        for (let i = inicioDia(); i > 0; i--) {
             dates.innerHTML += `<div class="date item ">
-            <button id="${obtenerDias(monthNumber-1)-(i-1)}" class="ui  disabled basic button">
-                ${obtenerDias(monthNumber-1)-(i-1)}
+            <button id="${obtenerDias(monthNumber - 1) - (i - 1)}" class="ui  disabled button">
+                ${obtenerDias(monthNumber - 1) - (i - 1)}
                 </button>
                 </div>`;
         }
         for (let i = 1; i <= obtenerDias(month); i++) {
             dates.innerHTML += `<div class="date">
-            <button id="${i}" class="ui disabled basic button">
+            <button id="${i}" class="ui disabled  button">
             ${i}
             </button>
             </div>`;
         }
     }
-    if( month === 3){
-        for(let i =inicioDia();i>0;i--){
+    if (month === 3) {
+        for (let i = inicioDia(); i > 0; i--) {
             dates.innerHTML += `<div class="date item ">
-            <button id="${obtenerDias(monthNumber-1)-(i-1)}" class="ui  disabled basic button">
-                ${obtenerDias(monthNumber-1)-(i-1)}
+            <button id="${obtenerDias(monthNumber - 1) - (i - 1)}" class="ui  disabled  button">
+                ${obtenerDias(monthNumber - 1) - (i - 1)}
                 </button>
                 </div>`;
         }
         for (let i = 1; i <= obtenerDias(month); i++) {
-            if(i === currentDay ){
+            if (i === currentDay) {
                 dates.innerHTML += `<div class="date item today">
-                <button id="${i}" class="ui purple basic button">
+                <button id="${i}" class="ui button">
                 ${i}
                 </button>
                 </div>`;
-                
-            }else if(i<currentDay){
+
+            } else if (i < currentDay) {
                 dates.innerHTML += `<div class="date">
-                <button id="${i}" class="ui disabled basic button">
+                <button id="${i}" class="ui disabled button">
                 ${i}
                 </button>
                 </div>`;
-            }else if(i>currentDay){
+            } else if (i > currentDay) {
                 dates.innerHTML += `<div class="date">
-                <button id="${i}"class="ui grey basic button">
+                <button id="${i}"class="ui grey button">
                 ${i}
                 </button>
                 </div>`;
             }
         }
-    }else if(month > 3){
-        for(let i =inicioDia();i>0;i--){
+    } else if (month > 3) {
+        for (let i = inicioDia(); i > 0; i--) {
             dates.innerHTML += `<div class="date item ">
-            <button id="${obtenerDias(monthNumber-1)-(i-1)}" class="ui  basic button">
-                ${obtenerDias(monthNumber-1)-(i-1)}
+            <button id="${obtenerDias(monthNumber - 1) - (i - 1)}" class="ui button">
+                ${obtenerDias(monthNumber - 1) - (i - 1)}
                 </button>
                 </div>`;
         }
         for (let i = 1; i <= obtenerDias(month); i++) {
             dates.innerHTML += `<div class="date">
-            <button id="${i}"class="ui grey basic button">
+            <button id="${i}"class="ui   button">
             ${i}
             </button>
             </div>`;
         }
+        
     }
-    
-    $(".ui.basic.button").click(function(e){
-        fecha= currentYear+'-'+month+'-'+$(this).attr("id");
-        console.log(fecha);
-    })
 
+    var contador = 0;
+    $(".ui.button").click(function (e) {
+        contador++;
+        //escribirMeses(monthNumber);
+        fecha = currentYear + '-' + month + '-' + $(this).attr("id");
+        console.log(fecha);
+        console.log(contador);
+        /*if (contador === 1) {
+            botonPresionado = e.currentTarget;
+            botonPresionado.className = ("ui blue basic button");
+            console.log(contador);
+            console.log(botonPresionado)
+        } else if (contador === 2) {
+            $(".dates").empty();
+            botonPresionado = e.currentTarget;
+            escribirMeses(monthNumber, botonPresionado);
+        }*/
+    })
 }
-function obtenerDias(month){
-    if(month === -1) month =11;
+
+function obtenerDias(month) {
+    if (month === -1) month = 11;
     if (month == 0 || month == 2 || month == 4 || month == 6 || month == 7 || month == 9 || month == 11) {
-        return  31;
+        return 31;
 
     } else if (month == 3 || month == 5 || month == 8 || month == 10) {
         return 30;
 
     } else {
 
-        return mesEspecial() ? 29:28;
+        return mesEspecial() ? 29 : 28;
     }
 }
-function mesEspecial(){//mes bisiesto
-    return((currentYear % 100 !==0) && (currentYear %4 === 0) || (currentYear % 400 ===0));
-    
+function mesEspecial() {//mes bisiesto
+    return ((currentYear % 100 !== 0) && (currentYear % 4 === 0) || (currentYear % 400 === 0));
+
 }
-function inicioDia(){//saber el dia 1 del mes
+function inicioDia() {//saber el dia 1 del mes
     let start = new Date(currentYear, monthNumber, 1);
-    if ((start.getDay()-1)=== -1){
+    if ((start.getDay() - 1) === -1) {
         return 6;
-    }else{
-        return start.getDay()-1;
-        
+    } else {
+        return start.getDay() - 1;
+
     }
     //return ((start.getDay()-1) === -1) ? 6 : start.getDay()-1;
 }
-function mesAnterior(){
-    if(monthNumber !== 0){
+function mesAnterior() {
+    if (monthNumber !== 0) {
         monthNumber--;
-    }else{
-        monthNumber= 11;
+    } else {
+        monthNumber = 11;
         currentYear--;
     }
     nuevaFecha();
 }
-function mesSiguiente(){
-    if(monthNumber !== 11){
+function mesSiguiente() {
+    if (monthNumber !== 11) {
         monthNumber++;
-    }else{
-        monthNumber= 0;
+    } else {
+        monthNumber = 0;
         currentYear++;
     }
     nuevaFecha();
 }
-function nuevaFecha(){
-    currentDate.setFullYear(currentYear,monthNumber,currentDay);
+function nuevaFecha() {
+    currentDate.setFullYear(currentYear, monthNumber, currentDay);
     month.textContent = monthNames[monthNumber];
     year.textContent = currentYear.toString();
     dates.textContent = '';
     escribirMeses(monthNumber);
+}
+
+
+var horario = document.getElementById('tardeMañana2');
+horario.style.display = "none";
+
+function selectHorario() {
+    var horas = document.getElementById("horas2").value;
+    if (horas !== "" ) {
+        horario.style.display = "block"
+    } else {
+        horario.style.display ="none"
+    }
 }
