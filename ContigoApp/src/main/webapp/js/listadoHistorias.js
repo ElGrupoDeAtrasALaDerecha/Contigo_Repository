6fd785a1086136f2b0e7ef001be18e9cc706c11b
LauraBlanco24 <laura.blanco01@correo.usa.edu.co>
2021-05-22 22:55:00 -0500

    Se agrega botones de ver y editar historia para personal calificado

--- ContigoApp/src/main/webapp/js/listadoHistorias.js
+++ ContigoApp/src/main/webapp/js/listadoHistorias.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
     usu = parseInt(getCookie("tipoUsuario"));
     if (usu !== 2) {
         document.getElementById('btnCrear').style.display = 'none';
+        
     }
 });
 
@@ -23,29 +24,59 @@ function cargarListaDeHistorias(){
                 listaHistorias=response.historias;
                 pintarHistorias();
             }
+            
         }, complete: function (result) {
-
+            //ocultarBotones();
         }, error: function (result) {
             alert("Error interno")
         }
     });
+    
 }
 
 function pintarHistorias(){
     for (let i = 0; i < listaHistorias.length; i++) {
         let historia =listaHistorias[i];
-        let txt=`<div id="${historia.id}" class="item">
-                    <div class="bottom">
-                        <a  class="textTitle">${historia.titulo}</a>
-                        <p class="text">${historia.descripcion}</p>
+        let txt=`
+                    <div id="${historia.id}" class="item">
+                    <div id="opacityImg" class="opacityImg">
+                        <div id="OpcBotones" class="OpcBotones">
+                            <a id="OpcBotonesEdita" class="OpcBotonesVer">Ver</a>
+                            <a class="OpcBotonesEditar">Editar</a>
                         </div>
-                </div>
+                        <div id="bottom" class="bottom" >
+                            <a  class="textTitle">${historia.titulo}
+                                <p class="text">${historia.descripcion}</p>
+                            </a>
+                        </div>
+                        <div>
+                    </div>
         `
+        
         $("#contenedorHistorias").append(txt);
         $("#"+historia.id).css('background-image', 'url(' + historia.urlImagen + ')');
         $("#"+historia.id).click(function(){
             setCookie("idHistoria",historia.id,0.1);
             window.location.assign("decisiones.html");
         })
+        ocultarBotones();
+    }
+    
+}
+   
+function ocultarBotones(){
+    usu = parseInt(getCookie("tipoUsuario"));
+    if (usu !== 2) {
+        let OpcBotones = document.getElementsByClassName('OpcBotones');
+        let opacityImg = document.getElementsByClassName('opacityImg');
+        let bottom = document.getElementsByClassName('bottom');
+        for (let index = 0; index < OpcBotones.length; index++) {
+            OpcBotones[index].style.display="none";
+            opacityImg[index].style.alignItems = 'Flex-end'
+            bottom[index].style.display = 'block';
+
+            
+        }
+        console.log("Holi")
     }
 }
