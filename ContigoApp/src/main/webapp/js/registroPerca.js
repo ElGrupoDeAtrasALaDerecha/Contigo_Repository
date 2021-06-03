e30e352e8fc0d59fad047e8bf985bbb72df5c952
santiagocaceres12 <santiago.caceres01@correo.usa.edu.co>
2021-06-03 16:16:09 -0500

    CONTIGO-170 Se actualiza la verificación de campos

--- ContigoApp/src/main/webapp/js/registroPerca.js
+++ ContigoApp/src/main/webapp/js/registroPerca.js
@@ -74,7 +74,7 @@ function crearPerca() {
 
 
     console.log(informacion);
-    
+
     $.ajax({
         url: "PersonalCalificado",
         type: "POST",
@@ -98,7 +98,7 @@ function crearPerca() {
         error: function (result) {
         }
     });
-    
+
 }
 
 function validar() {
@@ -120,15 +120,15 @@ function validar() {
     if (doc == "" || pNombre == "" || pApellido == "" || correo == "" || fechaNacimiento == "" || sApellido == "" || con == "" || gen == "" || conficontra == "" || img == "" || tipo == "Tipo" || gen == "Genero") {
 
         if (pNombre == "") {
-            document.getElementsByClassName("item form-floating mb-4")[0].setAttribute('data-error', 'Campo Obligatorio')
+            document.getElementsByClassName("item form-floating mb-4")[1].setAttribute('data-error', 'Campo Obligatorio')
             toastr.warning("Porfavor ingrese su nombre")
         }
         if (pApellido == "") {
-            document.getElementsByClassName("item form-floating mb-4")[1].setAttribute('data-error', 'Campo Obligatorio')
+            document.getElementsByClassName("item form-floating mb-4")[2].setAttribute('data-error', 'Campo Obligatorio')
             toastr.warning('Porfavor ingrese su primer apellido')
         }
         if (doc == "") {
-            document.getElementsByClassName("item form-floating mb-4")[2].setAttribute('data-error', 'Campo Obligatorio')
+            document.getElementsByClassName("item form-floating mb-4")[0].setAttribute('data-error', 'Campo Obligatorio')
             toastr.warning("Falta ingresar el documento")
             toastr.warning('Por favor ingrese su número de documento')
         }
@@ -136,31 +136,36 @@ function validar() {
             document.getElementsByClassName("item form-floating mb-4")[3].setAttribute('data-error', 'Campo Obligatorio')
             toastr.warning('Porfavor ingrese una fecha de nacimiento')
         }
-        if (con == "") {
+        if (gen == "Genero") {
             document.getElementsByClassName("item form-floating mb-4")[4].setAttribute('data-error', 'Campo Obligatorio')
+            toastr.warning('Debe seleccionar un genero')
+        }
+        if (con == "") {
+            document.getElementsByClassName("item form-floating mb-4")[5].setAttribute('data-error', 'Campo Obligatorio')
             toastr.warning('Debe ingresar una contraseña')
         }
         if (sApellido == "") {
-            document.getElementsByClassName("item form-floating mb-4")[6].setAttribute('data-error', 'Campo Obligatorio')
-            toastr.warning('Debe ingresar su segundo apellido')
-        }
-        if (correo == "") {
             document.getElementsByClassName("item form-floating mb-4")[7].setAttribute('data-error', 'Campo Obligatorio')
-            toastr.warning('Debe ingresar un correo')
+            toastr.warning('Debe ingresar su segundo apellido')
         }
         if (img == "") {
             document.getElementsByClassName("item form-floating mb-4")[8].setAttribute('data-error', 'Campo Obligatorio')
             toastr.warning('Porfavor suba una imagen de perfil')
         }
-        if (conficontra == "") {
+        if (correo == "") {
             document.getElementsByClassName("item form-floating mb-4")[9].setAttribute('data-error', 'Campo Obligatorio')
-            toastr.warning('Falta la confirmación de la contraseña')
+            toastr.warning('Debe ingresar un correo')
         }
-        if (gen == "") {
-            toastr.warning('Debe seleccionar un genero')
+        if (conficontra == "") {
+            document.getElementsByClassName("item form-floating mb-4")[10].setAttribute('data-error', 'Campo Obligatorio')
+            toastr.warning('Falta la confirmación de la contraseña')
         }
         if (con.length < 8 || conficontra.length < 8) {
             toastr.warning('la contraseña debe ser de minimo 8 caracteres')
+        }
+        if (tipo == "Tipo") {
+            document.getElementsByClassName("item form-floating")[0].setAttribute('data-error', 'Campo Obligatorio');
+            toastr.warning('Debe seleccionar un tipo de documento')
         }
     } else if (con !== conficontra) {
         toastr.error('Las contraseñas no coinciden')
@@ -168,33 +173,40 @@ function validar() {
         toastr.error('Correo no valido')
     } else {
         crearPerca();
+
     }
 }
 
 
 $("#dv0").click(function () {
-    document.getElementsByClassName("item form-floating mb-4")[0].removeAttribute('data-error')
-});
-$("#dv1").click(function () {
     document.getElementsByClassName("item form-floating mb-4")[1].removeAttribute('data-error')
 });
-$("#dv2").click(function () {
+$("#dv1").click(function () {
     document.getElementsByClassName("item form-floating mb-4")[2].removeAttribute('data-error')
+});
+$("#dv2").click(function () {
+    document.getElementsByClassName("item form-floating mb-4")[0].removeAttribute('data-error')
 });
 $("#dv3").click(function () {
     document.getElementsByClassName("item form-floating mb-4")[3].removeAttribute('data-error')
 });
 $("#dv4").click(function () {
-    document.getElementsByClassName("item form-floating mb-4")[4].removeAttribute('data-error')
+    document.getElementsByClassName("item form-floating mb-4")[5].removeAttribute('data-error')
 });
 $("#dv6").click(function () {
-    document.getElementsByClassName("item form-floating mb-4")[6].removeAttribute('data-error')
-});
-$("#dv7").click(function () {
     document.getElementsByClassName("item form-floating mb-4")[7].removeAttribute('data-error')
 });
-$("#dv9").click(function () {
+$("#dv7").click(function () {
     document.getElementsByClassName("item form-floating mb-4")[9].removeAttribute('data-error')
+});
+$("#dv9").click(function () {
+    document.getElementsByClassName("item form-floating mb-4")[10].removeAttribute('data-error')
+});
+$("#dv10").click(function () {
+    document.getElementsByClassName("item form-floating")[0].removeAttribute('data-error')
+});
+$("#dv11").click(function () {
+    document.getElementsByClassName("item form-floating mb-4")[4].removeAttribute('data-error')
 });
 
 $(document).ready(function () {
@@ -207,9 +219,9 @@ $(document).ready(function () {
                 console.log(result);
                 identificaciones = result.Identificaciones;
                 let tipos = '<option value ="' + identificaciones[0].id + '">' + identificaciones[0].tipo +
-                    '</option>'+
+                    '</option>' +
                     '<option value ="' + identificaciones[2].id + '">' + identificaciones[2].tipo +
-                    '</option>'+
+                    '</option>' +
                     '<option value ="' + identificaciones[3].id + '">' + identificaciones[3].tipo +
                     '</option>';
                 $("#tipoDocumento").append(tipos);
