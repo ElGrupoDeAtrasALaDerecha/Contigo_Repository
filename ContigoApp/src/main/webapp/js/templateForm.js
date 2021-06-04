e05756a03fed353cb0b7810eedb3d89beba2d76c
santiagocaceres12 <santiago.caceres01@correo.usa.edu.co>
2021-06-04 17:08:56 -0500

    Se arregla problema con el footer

---
+++ ContigoApp/src/main/webapp/js/templateForm.js
@@ -0,0 +1,99 @@
+
+$(document).ready(function () {
+    const menuSuperior = `
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="opciones.html">Contigo</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <div class="minav">
+                        <div class="navitems">
+                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="decisiones.html">Decisiones</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="citas.html">Citas</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="Conversatorios.html">Conversatorios</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <a href="index.html">
+                        <div class="log-out" id="salir">
+                            Salir
+                            <ion-icon name="log-out-outline"></ion-icon>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </nav>`
+
+    $('body').prepend(menuSuperior)
+
+    const footer =  `
+    <div class="box-1">
+        <div class="logoForm">
+            <a href="#"><p class="title">Contigo</p></a>
+        </div>
+        <div class="box-11">
+            <div class="icon">    
+                <a href="#"><img class="sepia" src="Iconos/facebook.png" alt=""></a>
+            </div>
+            <div class="icon">
+                <a href="#"><img class="sepia" src="Iconos/instagram.png"></a>
+            </div>
+            <div class="icon">
+                <a href="#"><img class="sepia" src="Iconos/linkedin.png"></a>
+            </div>
+            <div class="icon">
+                <a href="#"><img class="sepia" src="Iconos/youtube.png"></a>
+            </div>
+        </div>
+        <div class="row">
+            <img id="lines" src="img/lines.png" alt="">
+        </div>
+    </div>
+    <div class="box-2">
+        <div class="rxw">
+            <div class="coll">
+                <a href="#"><p class="option">Servicios</p></a> 
+                <a href="#"><p class="itemForm">Conversatorios</p></a> 
+                <a href="#"><p class="itemForm">Historias de decisión</p></a> 
+                <a href="#"><p class="itemForm">Chat privado</p></a> 
+                <a href="#"><p class="itemForm">Citas de ayuda</p></a> 
+            </div>
+            <div class="coll">
+                <a href="#"><p class="option">About</p></a> 
+                <a href="#"><p class="itemForm">Desarrolladores</p></a> 
+                <a href="#"><p class="itemForm">Personal calificado</p></a> 
+                <a href="#"><p class="itemForm">Contacto</p></a> 
+            </div>
+            <div class="coll">
+                <a href="#"><p class="option">Soporte</p></a> 
+                <a href="#"><p class="itemForm">Términos & Condiciones</p></a> 
+                <a href="#"><p class="itemForm">Política de Privacidad</p></a> 
+                <a href="#"><p class="itemForm">Preguntas frecuentes</p></a> 
+            </div>
+        </div>
+        <p class="itemForm" id="copyright">Copyright © 2021 Contigo. Creado por El Grupo De Atrás A La Derecha</p>
+    </div>
+
+    `
+    $('footer').append(footer)
+    $('body').append(`<script src="https://unpkg.com/ionicons@5.0.0/dist/ionicons.js"></script>`)
+    $("#salir").click(function(){
+        cerrarSesion();
+    });
+})
+
+function cerrarSesion() {
+    delete_cookie("token");
+    delete_cookie("tipoUsuario");
+    delete_cookie("documento");
+    window.location.assign("index.html");
+}
