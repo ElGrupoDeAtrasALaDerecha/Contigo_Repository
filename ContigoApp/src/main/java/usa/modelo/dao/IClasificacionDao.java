c0dc4130c9e34e7f7bc4f0da651f325a93b37e00
Santiago Pérez <santiago.perez01@correo.usa.edu.co>
2021-06-03 19:51:23 -0500

    Se agrega interfaz de clasificación dao

---
+++ ContigoApp/src/main/java/usa/modelo/dao/IClasificacionDao.java
@@ -0,0 +1,14 @@
+package usa.modelo.dao;
+
+import usa.modelo.dto.Clasificacion;
+
+/**
+ * Interfaz de objeto de acceso a datos de las clasificaciones
+ * @author Santiago Pérez
+ * @version 1.0
+ * @since 2021-06-03
+ * 
+ */
+public interface IClasificacionDao extends IDao<Clasificacion> {
+
+}
